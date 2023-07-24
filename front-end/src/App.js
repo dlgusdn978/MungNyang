6248eb2935d57a153cfd1ba432c4c7142baad456
lee hyunwoo <dlgusdn978@naver.com>
2023-07-24 17:52:46 +0900

    Feat : video Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import GlobalStyle from "./components/style/GlobalStyle";
-
+import VideoComponent from "./components/VideoBoxing";
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
             <span>도(그)라이(어 게임)</span>
+            <VideoComponent width="300px" height="300px" />
         </div>
     );
 }
