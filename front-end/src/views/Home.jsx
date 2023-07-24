2aa21539d0eed23cd01307422238a5960289a784
권영재 <dwdjjj@naver.com>
2023-07-24 16:50:57 +0900

    Feat: button component init

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "../components/Button";
 
 const Home = () => {
     return (
         <div>
             <h1>홈페이지</h1>
+            <Button text="test" />
         </div>
     );
 };
