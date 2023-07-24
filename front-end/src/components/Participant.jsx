d8442c390c710e47810f7109ed5f23b420efb074
Juaaang <darkssg@naver.com>
2023-07-24 16:13:47 +0900

    FE_Style: 참가자 컴포넌트 생성(#S09P12C209-123)

---
+++ front-end/src/components/Participant.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { styled } from "styled-components";
+
+function Participant({ user_list }) {
+    const host = "권영재";
+
+    const Container = styled.div`
+        width: 300px;
+        height: 300px;
+        background-color: #f1ead2;
+        font-size: 32px;
+        border-radius: 20px;
+        padding-left: 10px;
+        padding-top: 10px;
+    `;
+
+    return (
+        <Container>
+            <div className="header">
+                <h1>참가자</h1>
+            </div>
+            <br />
+            <div className="container-body">
+                {user_list.map((user, index) => (
+                    <React.Fragment key={index}>
+                        <span>{user} </span>
+                        {user === host ? null : <button>X</button>}
+                        <br />
+                    </React.Fragment>
+                ))}
+            </div>
+        </Container>
+    );
+}
+
+export default Participant;
