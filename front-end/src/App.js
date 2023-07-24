e3faf10ba122b2ad0a60f45a51b40e39f89451cf
권영재 <dwdjjj@naver.com>
2023-07-24 13:47:07 +0900

    Style: font setting

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,12 @@
-import "./css/App.css";
+import GlobalStyle from "./components/style/GlobalStyle";
 
 function App() {
-    return <div className="App" />;
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <span>도(그)라이(어 게임)</span>
+        </div>
+    );
 }
 
 export default App;
