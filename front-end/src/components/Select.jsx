03bfd65048ce0eca76c4efb0be94e54638fad833
LeeMinGyu <lmg386411@gmail.com>
2023-07-25 10:34:11 +0900

    Style:전체적인 크기 조정 및 폰트 크기 조절

--- front-end/src/components/Select.jsx
+++ front-end/src/components/Select.jsx
@@ -2,38 +2,38 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-    padding-top: 10px;
-    padding-left: 20px;
-    padding-right: 20px;
-    padding-bottom: 10px;
-    width: 200px;
-    height: 200px;
+    padding: 20px;
+    width: 600px;
+    height: 600px;
     background: #f2ead3;
     text-align: center;
 `;
 const Head = styled.div`
-    padding-bottom: 15px;
+    padding-bottom: 30px;
+    font-size: 32px;
 `;
 const Line = styled.div`
     margin-bottom: 5px;
+    padding-left: 10px;
     display: grid;
-    grid-template-columns: 70px 70px 70px;
+    grid-template-columns: 210px 210px 210px;
 `;
 const Content = styled.button`
-    width: 50px;
-    font-size: 8px;
+    width: 150px;
+    height: 50px;
+    font-size: 16px;
     background-color: #f5f5f5;
     border-color: #f5f5f5;
-    margin-bottom: 10px;
+    margin-bottom: 30px;
     border-radius: 5px;
 `;
 
 const Select = (props) => {
-    const { list, maxButtons } = props;
+    const { list, maxButtons, title } = props;
     const buttonList = list.slice(0, maxButtons);
     return (
         <Container>
-            <Head>제시어 카테고리</Head>
+            <Head>{title}</Head>
             <Line>
                 {buttonList.map((item, index) => (
                     <Content key={index}>{item}</Content>
--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Select from "../components/Select";
 
 const Home = () => {
-    const names = ["드라마", "영화", "스포츠"];
-    const nameList = names.map((name) => <Select name={name} />);
-    return <div>{nameList}</div>;
+    return (
+        <div>
+            <h1>홈페이지</h1>
+        </div>
+    );
 };
 
 export default Home;
