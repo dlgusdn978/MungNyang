785c4fa5de687f662c0be32aab8267d0c0cb8b00
권영재 <dwdjjj@naver.com>
2023-07-25 13:52:08 +0900

    Style : modify error

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Button from "../components/Button";
 
 const Home = () => {
     return (
         <div>
             <h1>홈페이지</h1>
-            <Button text="test" />
         </div>
     );
 };
