21abc52d4881bc5da8af7a81aceaf49d2d17c21b
LeeMinGyu <lmg386411@gmail.com>
2023-07-26 11:58:24 +0900

    Style : Home 화면 변경한 것 되돌리기

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Card from "../components/Card";
-import imgA from "../assets/img/clock.png";
 
 const Home = () => {
-    const description = "정답자가 정답을 입력 중입니다...";
-    return <Card description={description} imageSrc={imgA}></Card>;
+    return (
+        <div>
+            <h1>홈페이지</h1>
+        </div>
+    );
 };
 
 export default Home;
