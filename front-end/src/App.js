8e5fd6562f93b4d264d9d105dc5559ac6c8f0a9c
lee hyunwoo <dlgusdn978@naver.com>
2023-07-26 17:48:11 +0900

    Feat : WordDescription page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import GlobalStyle from "./components/style/GlobalStyle";
 import "./css/App.css";
-
+import "./views/WordDescription";
+import WordDescription from "./views/WordDescription";
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
+            test
             <span>도(그)라이(어 게임)</span>
+            <WordDescription />
         </div>
     );
 }
