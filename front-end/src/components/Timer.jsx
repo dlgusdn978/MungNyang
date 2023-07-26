9623eec43cab84b6041deaf125b84430a81f4256
lee hyunwoo <dlgusdn978@naver.com>
2023-07-26 12:03:26 +0900

    Style: timer css modify

--- front-end/src/components/Timer.jsx
+++ front-end/src/components/Timer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PuppyRun from "../assets/img/Puppy-run.gif";
 import ProgressTimer from "react-progress-bar-timer";
-import "../css/style//timer.css";
+import "../css/style/timer.css";
 const Timer = (props) => {
     const time = props;
     return (
