68b5ed2752c2c99363e7014a46824a5017124101
LeeMinGyu <lmg386411@gmail.com>
2023-07-26 15:07:43 +0900

    Style : home 화면 되돌리기

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,26 +1,11 @@
 import React from "react";
-import PonitedPersonAnswer from "./components/PonitedPersonAnswer";
 
 const Home = () => {
-    const title = "아래 단어들 중 정답을 골라 주세요";
-    const Answerlst = [
-        "고구마",
-        "토마토",
-        "날치알",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-        "짜게치",
-    ];
-    return <PonitedPersonAnswer title={title} Answerlst={Answerlst} />;
+    return (
+        <div>
+            <h1>홈페이지</h1>
+        </div>
+    );
 };
 
 export default Home;
