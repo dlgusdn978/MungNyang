84b7301c6b1335cc4f3f67700a4df4de23bbd2ef
LeeMinGyu <lmg386411@gmail.com>
2023-07-26 15:02:15 +0900

    Feat : 지목당한 사람이 정답 맞추는 화면

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import PonitedPersonAnswer from "./components/PonitedPersonAnswer";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>홈페이지</h1>
-        </div>
-    );
+    const title = "아래 단어들 중 정답을 골라 주세요";
+    const Answerlst = [
+        "고구마",
+        "토마토",
+        "날치알",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+        "짜게치",
+    ];
+    return <PonitedPersonAnswer title={title} Answerlst={Answerlst} />;
 };
 
 export default Home;
