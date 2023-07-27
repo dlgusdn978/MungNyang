f51c5c4546b2ecaff5f5b88a0390b1026759b30e
lee hyunwoo <dlgusdn978@naver.com>
2023-07-27 09:33:11 +0900

    Style : temporary border delete

--- front-end/src/views/WordDescription.js
+++ front-end/src/views/WordDescription.js
@@ -3,26 +3,23 @@ import styled from "styled-components";
 import VideoBoxing from "../components/VideoBoxing";
 import Button from "../components/Button";
 import CameraIcon from "../assets/img/camera.png";
+
 const Container = styled.div`
     margin: 0;
-    border: 1px solid black;
 `;
 const Participants = styled.div`
     width: ${(props) => (props.width ? props.width : "100%")};
     height: ${(props) => (props.height ? props.height : "500px")};
-    border: ${(props) => (props.border ? props.border : "1px solid black")};
     display: flex;
 `;
 const CurParticipants = styled.div`
     width: ${(props) => (props.width ? props.width : "50%")};
     height: ${(props) => (props.height ? props.height : "480px")};
-    border: ${(props) => (props.border ? props.border : "1px solid black")};
     margin: ${(props) => (props.margin ? props.margin : "10px")};
 `;
 const CurFunction = styled.div`
     width: ${(props) => (props.width ? props.width : "95%")};
     height: ${(props) => (props.height ? props.height : "60%")};
-    border: ${(props) => (props.border ? props.border : "1px solid black")};
     margin: ${(props) => (props.margin ? props.margin : "5px")};
     box-sizing: border-box;
 `;
@@ -30,7 +27,6 @@ const CurSubFunction = styled.div`
     display: flex;
     width: ${(props) => (props.width ? props.width : "95%")};
     height: ${(props) => (props.height ? props.height : "45%")};
-    border: ${(props) => (props.border ? props.border : "1px solid black")};
     margin: ${(props) => (props.margin ? props.margin : "5px")};
     box-sizing: border-box;
 `;
@@ -38,12 +34,10 @@ const CurSubBtn = styled.div`
     width: ${(props) => (props.width ? props.width : "60%")};
     height: 90%;
     margin: ${(props) => (props.margin ? props.margin : "3px")};
-    border: 1px solid black;
 `;
 const WaitingParticipants = styled.div`
     width: 20%;
     margin: 5px;
-    border: 1px solid black;
     border-radius: 5px;
 `;
 const user_list = ["권영재", "김대홍", "손임현", "이민규", "홍주영"];
@@ -70,7 +64,7 @@ function WordDescription() {
                         <CurSubFunction>
                             <CurSubBtn>
                                 <Button
-                                    width={"100%"}
+                                    width={"70%"}
                                     height={"100%"}
                                     text={"정답"}
                                     fontSize={"32px"}
