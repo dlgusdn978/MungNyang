f4827deff127797a00dcd2855b72a77c58e31a7e
SonImHyeon <soh2254@gmail.com>
2023-07-27 16:37:30 +0900

    Style : GameRoomDto ex추가

--- mung-BE/src/main/java/com/mung/mung/db/repository/GameRoomRepositoryImpl.java
+++ mung-BE/src/main/java/com/mung/mung/db/repository/GameRoomRepositoryImpl.java
@@ -8,10 +8,12 @@ import lombok.RequiredArgsConstructor;
 
 import static com.mung.mung.db.entity.QGameRoom.gameRoom;
 import java.util.List;
+
 @RequiredArgsConstructor
 public class GameRoomRepositoryImpl implements GameRoomRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
+    // queryFactory 내부 메서드 들을 사용하기 위함
 //    QGameRoom qGameRoom = QGameRoom.gameRoom;
     @Override
     public GameRoomDto findByTitle(String roomTitle) {
@@ -21,4 +23,13 @@ public class GameRoomRepositoryImpl implements GameRoomRepositoryCustom{
                 .where(gameRoom.roomTitle.eq(roomTitle))
                 .fetchOne();
     }
+
+    // @Override
+    // public GameRoomDto findByPk(long roomPw){
+    //     return queryFactory
+    //             .select(new QGameRoomDto(gameRoom.roomTitle, gameRoom.roomPw, gameRoom.roomUrl))
+    //             .from(gameRoom)
+    //             .where(gameRoom.roomPw.eq(roomPw))
+    //             .fetchOne();
+    // }
 }
