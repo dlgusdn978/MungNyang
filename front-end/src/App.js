26c3502e143e7984dea03082e576dc235d4d4a0d
lee hyunwoo <dlgusdn978@naver.com>
2023-07-31 17:57:26 +0900

    Style: username style modify

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,11 +4,13 @@ import Router from "./router/Router";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import "./assets/fonts/font.css";
+import QNA from "./views/components/QnAPage";
 
 function App() {
     return (
         <Provider store={store}>
             <GlobalStyle />
+            <QNA></QNA>
             <Router />
         </Provider>
     );
