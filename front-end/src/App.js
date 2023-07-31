58360133e9c82d926bc10ddfc05fec749d88ce5f
lee hyunwoo <dlgusdn978@naver.com>
2023-07-31 14:16:42 +0900

    Style: Timer disabled

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,13 +4,11 @@ import Router from "./router/Router";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import "./assets/fonts/font.css";
-import Timer from "./components/Timer";
 
 function App() {
     return (
         <Provider store={store}>
             <GlobalStyle />
-            <Timer />
             <Router />
         </Provider>
     );
