a2796fb7ef25542a277fcc78c3552927a3eaf031
권영재 <dwdjjj@naver.com>
2023-07-31 16:49:40 +0900

    Feat : redux modal setting

---
+++ front-end/src/components/modal/ReadyModal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ModalView } from "../layout";
+
+const ReadyModal = () => {
+    return (
+        <ModalView>
+            <h1>Are you Ready?</h1>
+        </ModalView>
+    );
+};
+
+export default ReadyModal;
