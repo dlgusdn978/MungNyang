5c40df7bd39cbae94902363105da2bd7ed986f42
권영재 <dwdjjj@naver.com>
2023-08-01 23:19:30 +0900

    Feat : homepage left side init

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,44 +1,14 @@
 import React, { useState } from "react";
-import Input from "../components/Input";
-import Button from "../components/Button";
-import { MainText, SubText } from "../components/layout/common";
-import {
-    ButtonBox,
-    FormBox,
-    HeaderBox,
-    HomeContainer,
-    LeftBox,
-    RightBox,
-} from "../components/layout/home";
+import { HomeContainer, RightBox } from "../components/layout/home";
+import CreateRoom from "./home/CreateRoom";
+import JoinRoom from "./home/JoinRoom";
 
 const Home = () => {
     const [view, setView] = useState(true);
 
     return (
         <HomeContainer>
-            {view ? (
-                <LeftBox className="leftbox">
-                    <HeaderBox>
-                        <MainText>멍 마을의 냥</MainText>
-                    </HeaderBox>
-                    <HeaderBox>
-                        <SubText>
-                            화상 서비스로 제시어를 몸으로 설명하고 맞추는
-                            라이어게임
-                        </SubText>
-                    </HeaderBox>
-                    <FormBox>
-                        <Input width="250px" />
-                        <Input type="password" width="250px" />
-                    </FormBox>
-                    <ButtonBox>
-                        <Button text={"방생성"} width="100px" margin="20px" />
-                        <Button text={"입장하기"} width="100px" margin="20px" />
-                    </ButtonBox>
-                </LeftBox>
-            ) : (
-                <LeftBox></LeftBox>
-            )}
+            {view ? <CreateRoom /> : <JoinRoom />}
             <RightBox className="rightbox" />
         </HomeContainer>
     );
