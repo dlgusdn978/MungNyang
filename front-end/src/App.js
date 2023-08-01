98f28bd7764857a04ab2da2e4ba0be9607b062d1
lee hyunwoo <dlgusdn978@naver.com>
2023-08-01 10:28:38 +0900

    Style: test code delete

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,13 +4,11 @@ import Router from "./router/Router";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import "./assets/fonts/font.css";
-import QNA from "./views/components/QnAPage";
 
 function App() {
     return (
         <Provider store={store}>
             <GlobalStyle />
-            <QNA></QNA>
             <Router />
         </Provider>
     );
