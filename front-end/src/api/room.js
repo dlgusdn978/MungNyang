50013a83097e4f3fea51d99935cb68e108a2e880
권영재 <dwdjjj@naver.com>
2023-08-02 15:25:02 +0900

    Feat : openvidu redux setting

--- front-end/src/api/room.js
+++ front-end/src/api/room.js
@@ -1,5 +1,7 @@
 import API from "./base";
 
-export const createRoom = () => {
-    API.post("/api/game-sessions");
+export const createRoom = (roomId, roomPw) => {
+    API.post(`/api/game-sessions`, { roomId: roomId, roomPw: roomPw })
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
 };
--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,31 +1,92 @@
 import React, { useState } from "react";
 import {
     ButtonBox,
+    FormBox,
+    HeaderBox,
     HomeContainer,
     LeftBox,
     RightBox,
 } from "../components/layout/home";
-import CreateRoom from "./home/CreateRoom";
-import JoinRoom from "./home/JoinRoom";
 import Button from "../components/Button";
+import { createRoom } from "../api/room";
+import { MainText, SubText } from "../components/layout/common";
+import Input from "../components/Input";
 
 const Home = () => {
     const [view, setView] = useState(false);
-    console.log(view);
+    const [roomInfo, setRoomInfo] = useState({
+        roomId: "",
+        roomPw: "",
+    });
+
+    const { roomId, roomPw } = roomInfo;
+
+    const handleChange = (e) => {
+        setRoomInfo({
+            ...roomInfo,
+            [e.target.id]: e.target.value,
+        });
+    };
+
+    const reset = () => {
+        setRoomInfo({
+            roomId: "",
+            roomPw: "",
+        });
+    };
 
     const changeView = () => {
+        setRoomInfo({ roomId: "", roomPw: "" });
         setView(!view);
+        // if (view) {
+        //     setRoomInfo({ roomId: "", roomPw: "" });
+        //     window.location.reload();
+        // }
+        console.log(roomInfo);
+    };
+    const makeRoom = () => {
+        console.log("방생성 api호출");
+        createRoom();
+    };
+    const joinRoom = () => {
+        console.log("방입장 api");
     };
 
     return (
         <HomeContainer>
             <LeftBox className="leftbox">
-                {view ? <CreateRoom /> : <JoinRoom />}
+                <HeaderBox>
+                    <MainText>멍 마을의 냥</MainText>
+                </HeaderBox>
+                <HeaderBox>
+                    <SubText>
+                        {view
+                            ? "화상 서비스로 제시어를 몸으로 설명하고 맞추는 라이어게임"
+                            : "방장에게 받은 방 아이디와 비밀번호를 입력하세요!"}
+                    </SubText>
+                </HeaderBox>
+                <FormBox>
+                    <Input
+                        id="roomId"
+                        width="250px"
+                        placeholder="방제목"
+                        value={roomId}
+                        onChange={handleChange}
+                    />
+                    <Input
+                        id="roomPw"
+                        width="250px"
+                        type="password"
+                        value={roomPw}
+                        onChange={handleChange}
+                    />
+                </FormBox>
                 <ButtonBox>
                     <Button
                         text={view ? "방생성" : "입장하기"}
                         width="100px"
                         margin="20px"
+                        onClick={view ? makeRoom : joinRoom}
                     />
                     <Button
                         text={view ? "입장하러가기" : "방생성하러가기"}
