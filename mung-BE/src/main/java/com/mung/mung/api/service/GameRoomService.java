b714304a2a372f229e3976b1382d7a1f1a173fae
SonImHyeon <soh2254@gmail.com>
2023-08-02 12:02:27 +0900

    Style : DB 저장 오류 해결

--- mung-BE/src/main/java/com/mung/mung/api/service/GameRoomService.java
+++ mung-BE/src/main/java/com/mung/mung/api/service/GameRoomService.java
@@ -1,4 +1,41 @@
 package com.mung.mung.api.service;
 
-public interface GameRoomService {
+import com.mung.mung.api.request.GameRoomCreateReq;
+import com.mung.mung.db.entity.Game;
+import com.mung.mung.db.entity.GameRoom;
+import com.mung.mung.db.repository.GameRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class GameRoomService {
+    private final GameRoomRepository gameRoomRepository;
+
+    @Autowired
+    public GameRoomService(GameRoomRepository gameRoomRepository){
+        this.gameRoomRepository=gameRoomRepository;
+    }
+
+    @Transactional
+    public boolean makeRoom(String roomId, final GameRoomCreateReq gameRoomCreateReq){
+        // DB에서 roomId가 있는지 검색 후 없으면 아래처럼 데이터 저장 후 생성, 있으면 return false
+        if (gameRoomRepository.findByRoomId(roomId).isPresent()) {
+            // 이미 해당 roomId가 존재하는 경우
+            return false;
+        } else
+        {
+
+            System.out.println(roomId);
+            GameRoom gameRoom = GameRoom.builder()
+                    .roomId(roomId)
+                    .roomPw(gameRoomCreateReq.getRoomPw())
+                    .startTime(LocalDateTime.now())
+                    .build();
+            gameRoomRepository.save(gameRoom);
+            return true;
+        }
+    }
 }
--- mung-BE/src/main/java/com/mung/mung/db/dao/GameRoomDAO.java
+++
@@ -1,8 +0,0 @@
-package com.mung.mung.db.dao;
-
-import com.mung.mung.db.entity.GameRoom;
-
-public interface GameRoomDAO {
-    GameRoom createGameRoom(GameRoom gameRoom);
-//    GameRoom getGameRoom(long gameId);
-}
