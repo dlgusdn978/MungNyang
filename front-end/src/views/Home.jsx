874569bb8f997b2d80a727fa582711a861ae26eb
권영재 <dwdjjj@naver.com>
2023-08-02 09:35:49 +0900

    Feat : change left view component

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
-import { HomeContainer, RightBox } from "../components/layout/home";
+import {
+    ButtonBox,
+    HomeContainer,
+    LeftBox,
+    RightBox,
+} from "../components/layout/home";
 import CreateRoom from "./home/CreateRoom";
 import JoinRoom from "./home/JoinRoom";
+import Button from "../components/Button";
 
 const Home = () => {
-    const [view, setView] = useState(true);
+    const [view, setView] = useState(false);
+    console.log(view);
+
+    const changeView = () => {
+        setView(!view);
+    };
 
     return (
         <HomeContainer>
-            {view ? <CreateRoom /> : <JoinRoom />}
+            <LeftBox className="leftbox">
+                {view ? <CreateRoom /> : <JoinRoom />}
+                <ButtonBox>
+                    <Button
+                        text={view ? "방생성" : "입장하기"}
+                        width="100px"
+                        margin="20px"
+                    />
+                    <Button
+                        text={view ? "입장하러가기" : "방생성하러가기"}
+                        onClick={changeView}
+                        width="100px"
+                        margin="20px"
+                    />
+                </ButtonBox>
+            </LeftBox>
             <RightBox className="rightbox" />
         </HomeContainer>
     );
