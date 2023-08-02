cd94f14205f4d827febd218d1493cc14c4ead86e
김대홍 <rlaeoghd1885@naver.com>
2023-08-02 12:03:49 +0900

    Style : join controller 수정

--- mung-BE/src/main/java/com/mung/mung/db/repository/GameRoomRepositoryCustom.java
+++ mung-BE/src/main/java/com/mung/mung/db/repository/GameRoomRepositoryCustom.java
@@ -8,4 +8,6 @@ import java.util.List;
 // Custom 에 사용할 메소드 정의 -> Impl 상세 쿼리 작성
 public interface GameRoomRepositoryCustom {
     GameRoomDto findByTitle(String roomTitle);
+
+    String findOwner(String roomId);
 }
