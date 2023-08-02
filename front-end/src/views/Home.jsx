bf1cc6b7cc82ba78b81692995d4573c93a62dadf
권영재 <dwdjjj@naver.com>
2023-08-02 12:01:26 +0900

    Style : rm timer

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -8,7 +8,6 @@ import {
 import CreateRoom from "./home/CreateRoom";
 import JoinRoom from "./home/JoinRoom";
 import Button from "../components/Button";
-import Timer from "../components/Timer";
 
 const Home = () => {
     const [view, setView] = useState(false);
@@ -20,7 +19,6 @@ const Home = () => {
 
     return (
         <HomeContainer>
-            <Timer />
             <LeftBox className="leftbox">
                 {view ? <CreateRoom /> : <JoinRoom />}
                 <ButtonBox>
