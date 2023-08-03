e0982f16220310bf1c6aea22cd38dce2e0bcad72
lee hyunwoo <dlgusdn978@naver.com>
2023-08-03 15:15:32 +0900

    Feat: timer library modify

--- front-end/src/components/Timer.jsx
+++ front-end/src/components/Timer.jsx
@@ -1,26 +1,27 @@
-import React from "react";
-import ProgressTimer from "react-progress-bar-timer";
+import React, { useState, useEffect } from "react";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
 import "../css/style/timer.css";
 const Timer = (props) => {
-    const { time } = props;
+    const time = props.time === "number" ? props.time : 10;
+    const decreaseRatio = 10 / time;
+    const [progress, setProgress] = useState(100);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((progress) => progress - decreaseRatio);
+        }, 100);
+
+        setTimeout(() => {
+            clearInterval(timer);
+        }, time * 1000);
+    }, [time, decreaseRatio]);
     return (
         <>
-            <ProgressTimer
-                barRounded
-                direction="left"
-                duration={typeof time === "number" ? time : 10}
-                fontSize={4}
-                rootRounded
-                started={true}
-                variant="empty"
-                disabled={"true"}
-                classes={{
-                    root: "root",
-                    progressContainer: "progressContainer",
-                    textContainer: "textContainer",
-                    progress: "progress",
-                }}
-            ></ProgressTimer>
+            <ProgressBar
+                percent={progress}
+                filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+            ></ProgressBar>
         </>
     );
 };
