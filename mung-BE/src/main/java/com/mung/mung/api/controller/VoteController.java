a59bb6383e127ce049192ddce9e1c6d2f5bb0b75
김대홍 <rlaeoghd1885@naver.com>
2023-08-03 17:59:30 +0900

    Style : 정답자 quiz 설계

--- mung-BE/src/main/java/com/mung/mung/api/controller/VoteController.java
+++ mung-BE/src/main/java/com/mung/mung/api/controller/VoteController.java
@@ -1,7 +1,7 @@
 package com.mung.mung.api.controller;
 
 import com.mung.mung.api.response.GameStartRes;
-import com.mung.mung.api.service.GameService;
+import com.mung.mung.api.service.VoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,30 +10,30 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/game")
-public class GameController {
+@RequestMapping("/api/vote")
+public class VoteController {
 
-    private final GameService gameService;
+    private final VoteService voteService;
 
-    @PostMapping("/startvote")
+    @PostMapping("/start")
     public ResponseEntity<GameStartRes> startVote(@RequestParam(required = true) String roomId) {
-        gameService.startVote(roomId);
+        voteService.startVote(roomId);
 
         return ResponseEntity.ok(new GameStartRes(roomId));
     }
 
-    @PostMapping("/vote")
+    @PostMapping("/count")
     public ResponseEntity<String> countVote(@RequestParam(required = true) String roomId) {
 
-        String result = gameService.countVote(roomId);
+        String result = voteService.countVote(roomId);
 
         return ResponseEntity.ok(result);
 
     }
 
-    @GetMapping("/voteResult")
+    @GetMapping("/result")
     public ResponseEntity<String> getVoteResult(@RequestParam String roomId) {
-        String result = gameService.getVoteResult(roomId);
+        String result = voteService.getVoteResult(roomId);
         return ResponseEntity.ok(result);
     }
 
