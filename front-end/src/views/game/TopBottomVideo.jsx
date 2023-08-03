d2cff47adb6a8b291ef53d460451fc996319f320
홍주영 <darkssg@naver.com>
2023-08-03 16:51:58 +0900

    Style : 레이아웃 컴포넌트 분리

--- front-end/src/views/game/TopBottomVideo.jsx
+++ front-end/src/views/game/TopBottomVideo.jsx
@@ -1,63 +1,19 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import {
+    Container,
+    HeaderBox,
+    VideoBox,
+    StateBox,
+    Footer,
+    AnswerBox,
+    FooterBox,
+} from "../../components/layout/topbottomvideo";
 import VideoComponent from "../../components/VideoBoxing";
 import Quiz from "../../components/Quiz";
 import Select from "../../components/Select";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-const Container = styled.div`
-    height: 720px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-`;
-
-const HeaderBox = styled.div`
-    width: 900px;
-    height: 150px;
-    display: flex;
-    justify-content: space-evenly;
-`;
-
-const VideoBox = styled.div`
-    width: 280px;
-    height: 150px;
-`;
-
-const StateBox = styled.div`
-    width: 700px;
-    height: 380px;
-    border-radius: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: var(--beige);
-`;
-
-const FooterBox = styled.div`
-    width: 900px;
-    height: 150px;
-    display: flex;
-    justify-content: space-evenly;
-`;
-const AnswerBox = styled.div`
-    width: 800px;
-    height: 380px;
-    background: var(--beige);
-    border-radius: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-const Footer = styled.div`
-    width: 500px;
-    display: flex;
-    justify-content: space-between;
-`;
 function TopBottomVideo() {
     const list = ["임시", "임시", "임시"];
     const title = "제시어 카테고리";
