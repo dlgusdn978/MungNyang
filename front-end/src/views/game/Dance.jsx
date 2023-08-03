7638de7608800cc4e713b75846ed3312d0492457
LeeMinGyu <lmg386411@gmail.com>
2023-08-03 10:25:43 +0900

    Style : 배치 조정, iframe에 border radius 적용, 알림창 띄우기

--- front-end/src/views/game/Dance.jsx
+++ front-end/src/views/game/Dance.jsx
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Button from "../../components/Button";
 import VideoComponent from "../../components/VideoBoxing";
 import { Box } from "../../components/layout/common";
 import Timer from "../../components/Timer";
+import {
+    NotificationContainer,
+    Overlay,
+} from "../../components/layout/selectAnswer";
 
-const Container = styled.div`
-    margin-top: 20px;
-`;
+const Container = styled.div``;
 const Body = styled.div`
     margin-top: 10px;
     display: flex;
 `;
 const Left = styled.div`
     flex: 1;
-    display: flex;
 `;
 const Right = styled.div`
-    flex: 2;
+    flex: 3;
+    margin-left: 10px;
+    display: flex;
 `;
 const Video = styled.div`
     margin-left: 30px;
     width: 253px;
     height: 450px;
     iframe {
+        border-radius: 1.5rem;
         width: 100%;
         height: 100%;
         object-fit: cover;
@@ -31,14 +35,27 @@ const Video = styled.div`
 `;
 const Btn = styled.div`
     margin-left: 20px;
+    margin-right: 5px;
     margin-top: 10px;
 `;
 const Users = styled.div`
-    margin-top: 10px;
+    margin-top: 20px;
 `;
 
 const Dance = (props) => {
     const { url, userlist, penalty } = props;
+    const [showNotification, setShowNotification] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowNotification(false);
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+
     return (
         <Container>
             <Timer></Timer>
@@ -53,16 +70,31 @@ const Dance = (props) => {
                             allow="autoplay"
                         ></iframe>
                     </Video>
-                    <Btn>
-                        <Button width="50px">PASS</Button>
-                        <Button width="50px">FAIL</Button>
-                    </Btn>
                 </Left>
                 <Right>
                     <VideoComponent
-                        width="830px"
+                        width="800px"
                         height="450px"
                     ></VideoComponent>
+                    <Btn>
+                        <Button
+                            width="100px"
+                            height="150px"
+                            margin="40px 0px"
+                            color="black"
+                            fontSize="32px"
+                        >
+                            PASS
+                        </Button>
+                        <Button
+                            width="100px"
+                            height="150px"
+                            color="black"
+                            fontSize="32px"
+                        >
+                            FAIL
+                        </Button>
+                    </Btn>
                 </Right>
             </Body>
             <Users>
@@ -72,6 +104,10 @@ const Dance = (props) => {
                     </Box>
                 ))}
             </Users>
+            <Overlay show={showNotification} />
+            <NotificationContainer show={showNotification}>
+                벌칙자 : {penalty}
+            </NotificationContainer>
         </Container>
     );
 };
