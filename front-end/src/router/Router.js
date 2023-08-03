219d8c767a58eeca5dad964d95ea13431df92bb4
권영재 <dwdjjj@naver.com>
2023-08-03 14:39:26 +0900

    Style : router rollback

--- front-end/src/router/Router.js
+++ front-end/src/router/Router.js
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
 import Game from "../views/Game";
-import QnAPage from "../views/game/QnAPage";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/game" element={<QnAPage />} />
+                <Route path="/game" element={<Game />} />
             </Routes>
         </BrowserRouter>
     );
