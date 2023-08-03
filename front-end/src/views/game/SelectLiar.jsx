79fe52fbfb048fa9862f42224998c69997536e04
LeeMinGyu <lmg386411@gmail.com>
2023-08-03 15:19:34 +0900

    Style : 태그 명 변경 & css layout으로 빼기 & 비율 및 크기 조정

--- front-end/src/views/game/SelectLiar.jsx
+++ front-end/src/views/game/SelectLiar.jsx
@@ -1,54 +1,21 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import VideoComponent from "../../components/VideoBoxing";
 import foot from "../../assets/img/foot.png";
 import Timer from "../../components/Timer";
-
-const Container = styled.div``;
-const Line = styled.div`
-    display: grid;
-    grid-template-columns: 430px 430px 430px;
-    margin-bottom: 20px;
-    margin-left: 50px;
-`;
-const Box = styled.div`
-    margin-top: 10px;
-    transition: all 0.2s ease-in-out;
-    position: relative;
-
-    &:hover {
-        transform: scale(1.1);
-        cursor: pointer;
-    }
-`;
-const NotificationContainer = styled.div`
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 9999;
-    background-color: rgba(0, 0, 0, 0.8);
-    padding: 10px 20px;
-    border-radius: 5px;
-    display: ${(props) => (props.show ? "block" : "none")};
-    transition: display 0.2s;
-    font-size: 72px;
-`;
-const ImageOverlay = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-80%, -50%);
-    font-size: 20px;
-    font-weight: bold;
-    display: ${(props) => (props.active ? "block" : "none")};
-`;
-const imgSrc = foot;
+import { Container } from "../../components/layout/common";
+import {
+    Box,
+    Item,
+    NotificationContainer,
+    ImageOverlay,
+} from "../../components/layout/selectLiar";
 
 const SelectLiar = (props) => {
-    const { userlist, text } = props;
+    const { userlist } = props;
     const [showNotification, setShowNotification] = useState(true);
     const [activeBox, setActiveBox] = useState(null);
+    const text = "라이어를 선택하세요.";
+    const imgSrc = foot;
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -68,19 +35,19 @@ const SelectLiar = (props) => {
     return (
         <Container>
             <Timer></Timer>
-            <Line>
+            <Box>
                 {userlist.map((boxIndex) => (
-                    <Box
+                    <Item
                         key={boxIndex}
                         onClick={() => handleBoxClick(boxIndex)}
                     >
                         <ImageOverlay active={activeBox === boxIndex}>
                             <img src={imgSrc} alt="사진" width="100%" />
                         </ImageOverlay>
-                        <VideoComponent width="300px" height="320px" />
-                    </Box>
+                        <VideoComponent width="350px" height="320px" />
+                    </Item>
                 ))}
-            </Line>
+            </Box>
             <NotificationContainer show={showNotification}>
                 {text}
             </NotificationContainer>
