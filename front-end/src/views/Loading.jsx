01a13d6ec4af70120523c71356a9dab3672ef0a6
LeeMinGyu <lmg386411@gmail.com>
2023-08-04 16:42:16 +0900

    Style : css 작업

--- front-end/src/views/Loading.jsx
+++ front-end/src/views/Loading.jsx
@@ -1,14 +1,32 @@
 import styled from "styled-components";
-import { Container } from "../components/layout/common";
 import smell from "../assets/img/smell.png";
+import cat from "../assets/img/cat.png";
+import loading from "../assets/img/loading.png";
 
-const ImgBox = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+const ImgBox = styled.div`
+    display: flex;
+`;
 const TextBox = styled.div``;
+const ImgItem = styled.div``;
 
 const Loading = () => {
     return (
         <Container>
-            <ImgBox></ImgBox>
+            <ImgBox>
+                <ImgItem>
+                    <img src={loading} alt="" />
+                </ImgItem>
+                <ImgItem>
+                    <img src={smell} alt="" />
+                </ImgItem>
+                <ImgItem>
+                    <img src={cat} alt="" />
+                </ImgItem>
+            </ImgBox>
             <TextBox></TextBox>
         </Container>
     );
