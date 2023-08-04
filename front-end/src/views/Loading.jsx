0ad0d5d8ed5d0ad00a7f32d28932d25e38b90839
LeeMinGyu <lmg386411@gmail.com>
2023-08-04 15:59:17 +0900

    Feat : 로딩 페이지 작업

---
+++ front-end/src/views/Loading.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Container } from "../components/layout/common";
+import smell from "../assets/img/smell.png";
+
+const ImgBox = styled.div``;
+const TextBox = styled.div``;
+
+const Loading = () => {
+    return (
+        <Container>
+            <ImgBox></ImgBox>
+            <TextBox></TextBox>
+        </Container>
+    );
+};
+export default Loading;
