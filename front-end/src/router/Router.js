8ae96e6ccf01be3dbd6d9383666e4cd1f5b0e108
권영재 <dwdjjj@naver.com>
2023-08-05 14:10:08 +0900

    Feat : waitingroom을 game페이지에서 분리

--- front-end/src/router/Router.js
+++ front-end/src/router/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
 import Game from "../views/Game";
 import Error from "../views/Error";
+import ConnectionTest from "../views/game/ConnectionTest";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/test" element={<ConnectionTest />} />
                 <Route path="/game" element={<Game />} />
                 <Route path="/error" element={<Error />} />
             </Routes>
--- front-end/src/views/game/WaitingRoom.jsx
+++ front-end/src/views/game/WaitingRoom.jsx
@@ -25,26 +25,61 @@ import { useDispatch, useSelector } from "react-redux";
 import { openModal } from "../../store/modalSlice";
 import { ovActions } from "../../store/openviduSlice";
 
-const user_list = ["권영재", "김대홍", "손임현", "이민규", "이현우", "홍주영"];
-
-const host = "권영재";
+// const user_list = ["권영재", "김대홍", "손임현", "이민규", "이현우", "홍주영"];
+// const host = "권영재";
 
 function WaitingRoom() {
     const [setCnt, setSetCnt] = useState(3);
-    const [users, setUsers] = useState([]);
     const [isMuted, setIsMuted] = useState(false);
     const openvidu = useSelector((state) => state.openvidu);
-    // setUsers(subscribers)
     const {
         session,
         subscribers,
         myUserName,
         mySessionId,
         mainStreamManager,
-        devices,
+        // devices,
+        token,
     } = openvidu;
-
+    console.log(subscribers);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        // 유저들을 OpenVidu 세션에 연결하는 함수
+        const connectUsersToSession = async () => {
+            try {
+                const usersToConnect = subscribers.filter(
+                    (user) => user !== mainStreamManager,
+                );
+
+                // 각 유저들을 세션에 연결
+                for (const user of usersToConnect) {
+                    await session.connect(token, { clientData: user });
+                }
+
+                // Subscribe to streams of connected users and update subscribers state
+                session.on("streamCreated", (event) => {
+                    const subscriber = session.subscribe(
+                        event.stream,
+                        undefined,
+                    );
+                    // Dispatch the action to update subscribers in Redux state
+                    dispatch(
+                        ovActions.saveSubscribers([...subscribers, subscriber]),
+                    );
+                });
+
+                console.log("Users connected to the session");
+            } catch (error) {
+                console.error("Error connecting users to the session:", error);
+            }
+        };
+
+        // 세션이 유효한 경우에만 유저들을 세션에 연결
+        if (session && subscribers) {
+            connectUsersToSession();
+        }
+    }, [session, token, subscribers, dispatch]);
 
     const openRuleBook = () => {
         dispatch(
@@ -85,7 +120,10 @@ function WaitingRoom() {
                 </VideoboxGrid>
             </Leftbox>
             <Rightbox>
-                <Participant user_list={user_list} host={host} />
+                <Participant
+                    user_list={subscribers}
+                    // host={host}
+                />
                 <ChattingBox>
                     <ChatBox>채팅내용...</ChatBox>
                     <ChattingInputBox>
