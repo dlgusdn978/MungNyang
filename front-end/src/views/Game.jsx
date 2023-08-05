270e01c829558664bede27684e20c5c7f03efe03
권영재 <dwdjjj@naver.com>
2023-08-05 12:01:55 +0900

    Feat : session join ...

--- front-end/src/views/Game.jsx
+++ front-end/src/views/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WaitingRoom from "./game/WaitingRoom";
 import ConnectionTest from "./game/ConnectionTest";
 import TopBottomVideo from "./game/TopBottomVideo";
@@ -54,11 +54,12 @@ const Game = () => {
         myUserName,
         mySessionId,
         mainStreamManager,
-        devices,
+        currentVideoDevice,
         token,
     } = openvidu;
+    console.log(openvidu);
+    const [OV, setOV] = useState();
     const [state, setState] = useState({
-        OV,
         mySessionId: mySessionId,
         myUserName: myUserName,
         session: undefined,
@@ -71,79 +72,124 @@ const Game = () => {
     const dispatch = useDispatch(); //dispatch로 reducer에 선언된 changePhase 불러와서 사용하면됨
     console.log(phaseType);
 
-    console.log(openvidu);
+    useEffect(function () {
+        // window.addEventListener("beforeunload", this.onbeforeunload);
+        // window.removeEventListener("beforeunload", this.onbeforeunload);
+        // useEffect가 한번 실행된 후 다시 실행될 때 이전에 실행했던 값을 정리해주는 것이 clean up
+        // 예를 들면 입장할 때 구독하고 퇴장할 때 구독취소를 하는 것
+        // 이 effect는 부모 컴포넌트에서 이 함수 컴포넌트를 삭제하는 경우에 clean-up(unmount)을 하게 된다.
+        // return function beforeunload() {
+        //     this.leaveSession();
+        // };
+        // 빈 배열을 넣으면 처음 한 번만 실행되고 그 후로는 실행되지 않는다.(componentDidMount만 하도록)
+    }, []);
 
-    var OV = new OpenVidu();
-    const session = OV.initSession();
-
-    if (session) {
-        // On every new Stream received...
-        session.on("streamCreated", (event) => {
-            // Subscribe to the Stream to receive it. Second parameter is undefined
-            // so OpenVidu doesn't create an HTML video by its own
-            var subscriber = session.subscribe(event.stream, undefined);
-
-            //We use an auxiliar array to push the new stream
-            var subscribers = this.state.subscribers;
-
-            subscribers.push(subscriber);
-
-            // Update the state with the new subscribers
-        });
-    }
-    console.log(subscribers);
-
-    token &&
-        session
-            .connect(token, "User 1")
-            .then(async () => {
-                // --- 5) Get your own camera stream ---
-
-                // Init a publisher passing undefined as targetElement (we don't want OpenVidu to insert a video
-                // element: we will manage it on our own) and with the desired properties
-                let publisher = await OV.initPublisherAsync(undefined, {
-                    audioSource: undefined, // The source of audio. If undefined default microphone
-                    videoSource: undefined, // The source of video. If undefined default webcam
-                    publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
-                    publishVideo: true, // Whether you want to start publishing with your video enabled or not
-                    resolution: "640x480", // The resolution of your video
-                    frameRate: 30, // The frame rate of your video
-                    insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
-                    mirror: false, // Whether to mirror your local video or not
-                });
-
-                // --- 6) Publish your stream ---
-
-                session.publish(publisher);
-
-                // Obtain the current video device in use
-                var devices = await OV.getDevices();
-                var videoDevices = devices.filter(
-                    (device) => device.kind === "videoinput",
-                );
-                var currentVideoDeviceId = publisher.stream
-                    .getMediaStream()
-                    .getVideoTracks()[0]
-                    .getSettings().deviceId;
-                var currentVideoDevice = videoDevices.find(
-                    (device) => device.deviceId === currentVideoDeviceId,
-                );
-
-                // Set the main video in the page to display our webcam and store our Publisher
-                setState({
-                    ...state,
-                    currentVideoDevice: currentVideoDevice,
-                    mainStreamManager: publisher,
-                    publisher: publisher,
-                });
-            })
-            .catch((error) => {
-                console.log(
-                    "There was an error connecting to the session:",
-                    error.code,
-                    error.message,
-                );
+    const deleteSubscriber = (streamManager) => {
+        const prevSubscribers = subscribers;
+        let index = prevSubscribers.indexOf(streamManager, 0);
+        if (index > -1) {
+            prevSubscribers.splice(index, 1);
+            setState({
+                ...state,
+                subscribers: [...prevSubscribers],
             });
+        }
+    };
+
+    const joinSession = () => {
+        const newOV = new OpenVidu();
+        newOV.enableProdMode();
+
+        const newSession = OV.initSession();
+
+        setOV(newOV);
+        setState({
+            ...state,
+            session: newSession,
+        });
+
+        const connection = () => {
+            if (token) {
+                // 1. connection 메소드 내부에 이벤트 수신 처리
+                // 1-1 session에 참여한 사용자 추가
+                newSession.on("streamCreated", (event) => {
+                    const newSubscriber = newSession.subscribe(
+                        event.stream,
+                        JSON.parse(event.stream.connection.data).clientData,
+                    );
+
+                    const newSubscribers = subscribers;
+                    newSubscribers.push(newSubscriber);
+
+                    setState({
+                        ...state,
+                        subscribers: [...newSubscribers],
+                    });
+                });
+                // 1-2 session에서 disconnect한 사용자 삭제
+                // newSession.on("streamDestroyed", (event) => {
+                //     if (event.stream.typeOfVideo === "CUSTOM") {
+                //         deleteSubscriber(event.stream.streamManager);
+                //     } else {
+                //         setDestroyedStream(event.stream.streamManager);
+                //         setCheckMyScreen(true);
+                //     }
+                // });
+                // 1-3 예외처리
+                newSession.on("exception", (exception) => {
+                    console.warn(exception);
+                });
+                newSession
+                    .connect(token, {
+                        clientData: myUserName,
+                    })
+                    .then(async () => {
+                        newOV
+                            .getUserMedia({
+                                audioSource: false,
+                                videoSource: undefined,
+                                resolution: "1280x720",
+                                frameRate: 10,
+                            })
+                            .then((mediaStream) => {
+                                var videoTrack =
+                                    mediaStream.getVideoTracks()[0];
+                                // Obtain the current video device in use
+
+                                var newPublisher = newOV.initPublisher(
+                                    myUserName,
+                                    {
+                                        audioSource: undefined,
+                                        videoSource: videoTrack,
+                                        publishAudio: true,
+                                        publishVideo: true,
+                                        // resolution: '1280x720',
+                                        // frameRate: 10,
+                                        insertMode: "APPEND",
+                                        mirror: true,
+                                    },
+                                );
+                                // 4-c publish
+                                newPublisher.once("accessAllowed", () => {
+                                    newSession.publish(newPublisher);
+                                    setState({
+                                        ...state,
+                                        publisher: newPublisher,
+                                    });
+                                });
+                            })
+                            .catch((error) => {
+                                console.warn(
+                                    "There was an error connecting to the session:",
+                                    error.code,
+                                    error.message,
+                                );
+                            });
+                    });
+            }
+        };
+        connection();
+    };
 
     const findPhase = PHASE_COMPONENTS.find(
         (phase) => phase.type === phaseType,
