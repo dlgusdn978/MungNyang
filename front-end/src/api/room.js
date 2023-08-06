e3c56d4d6d01da024fa5634d507000908cb3af98
권영재 <dwdjjj@naver.com>
2023-08-06 13:55:34 +0900

    Style : Game.jsx 수정실패

--- front-end/src/api/room.js
+++ front-end/src/api/room.js
@@ -5,6 +5,7 @@ export const createRoom = (roomId, roomPw) => {
     return API.post(`/api/game-sessions`, { roomId: roomId, roomPw: roomPw });
 };
 
+// createRoom으로 만든 sessionId에 해당하는 방에 연결 -> 반환 : 토큰값
 export const connectRoom = (sessionId, roomPw) => {
     console.log(sessionId);
     return API.post(`/api/game-sessions/${sessionId}/connections`, {
