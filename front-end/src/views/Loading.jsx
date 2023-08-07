3320d3851794340854b5f24366233fca81154ec3
LeeMinGyu <lmg386411@gmail.com>
2023-08-07 14:05:18 +0900

    Style : css layout으로 빼서 코드 간략화

--- front-end/src/views/Loading.jsx
+++ front-end/src/views/Loading.jsx
@@ -1,39 +1,11 @@
-import styled, { keyframes } from "styled-components";
 import imgSrc from "../assets/img/loadingback.png";
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    background-color: var(--dusty-pink-light);
-`;
-const TextBox = styled.div`
-    display: flex;
-    justify-content: space-between;
-    font-size: 32px;
-    color: var(--brown-dark);
-    margin-top: 10px;
-`;
-const TipItem = styled.div`
-    margin-left: 10px;
-`;
-const LoadingItem = styled.div`
-    margin: 0px 20px 20px 0px;
-    display: inline-block;
-`;
-const jumpAnimation = keyframes`
-    0%, 100% {
-        transform: translateY(0);
-    }
-    50% {
-        transform: translateY(-10px);
-    }
-`;
-const LoadingText = styled.span`
-    display: inline-block;
-    animation: ${jumpAnimation} 2.5s infinite;
-    animation-delay: ${(props) => props.delay}s;
-`;
+import {
+    Container,
+    TextBox,
+    TipItem,
+    LoadingItem,
+    LoadingText,
+} from "../components/layout/loading";
 
 const Loading = () => {
     return (
