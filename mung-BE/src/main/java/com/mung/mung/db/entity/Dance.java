c67b138f550ceb586e6372d2108b99a75bcf446a
김대홍 <rlaeoghd1885@naver.com>
2023-08-08 00:36:35 +0900

    Feat : Liar Controller 기능 완성

--- mung-BE/src/main/java/com/mung/mung/db/entity/Dance.java
+++ mung-BE/src/main/java/com/mung/mung/db/entity/Dance.java
@@ -9,11 +9,11 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
 @Getter
-@Setter
 @Entity
 @Builder
 @AllArgsConstructor
@@ -21,7 +21,7 @@ import javax.persistence.Id;
 public class Dance {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long danceId;
 
     private String danceUrl;
