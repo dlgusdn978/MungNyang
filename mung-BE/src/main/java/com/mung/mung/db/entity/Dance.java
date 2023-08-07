dbb3941225d9c671783101a06169f7464a260423
김대홍 <rlaeoghd1885@naver.com>
2023-08-07 17:20:25 +0900

    Style : Entity 수정

--- mung-BE/src/main/java/com/mung/mung/db/entity/Dance.java
+++ mung-BE/src/main/java/com/mung/mung/db/entity/Dance.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
@@ -21,7 +22,7 @@ import javax.persistence.Id;
 public class Dance {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long danceId;
 
     private String danceUrl;
