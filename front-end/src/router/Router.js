2b6bab62489543f6885c972174954dd14bc33075
LeeMinGyu <lmg386411@gmail.com>
2023-08-07 13:55:46 +0900

    Style : error 라우터 경로 추가

--- front-end/src/router/Router.js
+++ front-end/src/router/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
 import Game from "../views/Game";
 import Loading from "../views/Loading";
+import Error from "../views/Error";
 import ConnectionTest from "../views/game/ConnectionTest";
 
 const Router = () => {
@@ -13,6 +14,7 @@ const Router = () => {
                 <Route path="/test" element={<ConnectionTest />} />
                 <Route path="/game" element={<Game />} />
                 <Route path="/loading" element={<Loading />} />
+                <Route path="/error" element={<Error />} />
             </Routes>
         </BrowserRouter>
     );
