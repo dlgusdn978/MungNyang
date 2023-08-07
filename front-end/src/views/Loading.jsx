a12b802988460700865ab2841a2d2d781e797e84
LeeMinGyu <lmg386411@gmail.com>
2023-08-07 13:51:46 +0900

    Style : loading 화면 css 작업

--- front-end/src/views/Loading.jsx
+++ front-end/src/views/Loading.jsx
@@ -1,68 +1,64 @@
-import styled from "styled-components";
-import smell from "../assets/img/smell.png";
-import cat from "../assets/img/cat.png";
-import loading from "../assets/img/loading.png";
-import imgSrc from "../assets/img/loadingBackground.png";
+import styled, { keyframes } from "styled-components";
+import imgSrc from "../assets/img/loadingback.png";
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     height: 100%;
-    justify-content: flex-end;
-    align-items: flex-end;
-    background-image: url("../assets/img/loadingBackground.png");
-    background-size: cover;
-`;
-const ImgBox = styled.div`
-    margin-right: 50px;
-    display: flex;
+    background-color: var(--dusty-pink-light);
 `;
 const TextBox = styled.div`
-    margin: 0px 50px 50px 0px;
-    font-size: 64px;
+    display: flex;
+    justify-content: space-between;
+    font-size: 32px;
+    color: var(--brown-dark);
+    margin-top: 10px;
 `;
-const ImgItem = styled.div`
-    animation: rotate 3s infinite;
-    @keyframes rotate {
-        0%,
-        100% {
-            transform: rotate(0deg);
-        }
-        50% {
-            transform: rotate(30deg);
-        }
-        75% {
-            transform: rotate(0deg);
-        }
+const TipItem = styled.div`
+    margin-left: 10px;
+`;
+const LoadingItem = styled.div`
+    margin: 0px 20px 20px 0px;
+    display: inline-block;
+`;
+const jumpAnimation = keyframes`
+    0%, 100% {
+        transform: translateY(0);
+    }
+    50% {
+        transform: translateY(-10px);
     }
 `;
-const DogItem = styled.div``;
+const LoadingText = styled.span`
+    display: inline-block;
+    animation: ${jumpAnimation} 2.5s infinite;
+    animation-delay: ${(props) => props.delay}s;
+`;
 
 const Loading = () => {
     return (
         <Container>
-            <img width={1280} height={500} src={imgSrc} alt="" />
-            <ImgBox>
-                <ImgItem>
-                    <img width={100} height={100} src={loading} alt="" />
-                </ImgItem>
-                <ImgItem>
-                    <img width={100} height={100} src={loading} alt="" />
-                </ImgItem>
-                <ImgItem>
-                    <img width={100} height={100} src={loading} alt="" />
-                </ImgItem>
-                <ImgItem>
-                    <img width={100} height={100} src={loading} alt="" />
-                </ImgItem>
-                <DogItem>
-                    <img width={100} height={100} src={smell} alt="" />
-                </DogItem>
-                <DogItem>
-                    <img width={100} height={100} src={cat} alt="" />
-                </DogItem>
-            </ImgBox>
-            <TextBox>Loading ...</TextBox>
+            <img width={1280} height={670} src={imgSrc} alt="" />
+            <TextBox>
+                <TipItem>Tip : 게임시작 전 간단한 스트레칭은 어떤가요?</TipItem>
+                <LoadingItem>
+                    <LoadingText delay={0.25}>N</LoadingText>
+                    <LoadingText delay={0.5}>o</LoadingText>
+                    <LoadingText delay={0.75}>w</LoadingText>
+                    <LoadingText delay={1}>&nbsp;</LoadingText>
+                    <LoadingText delay={1.25}>L</LoadingText>
+                    <LoadingText delay={1.5}>o</LoadingText>
+                    <LoadingText delay={1.75}>a</LoadingText>
+                    <LoadingText delay={2}>d</LoadingText>
+                    <LoadingText delay={2.25}>i</LoadingText>
+                    <LoadingText delay={2.5}>n</LoadingText>
+                    <LoadingText delay={2.75}>g</LoadingText>
+                    <LoadingText delay={3}>&nbsp;</LoadingText>
+                    <LoadingText delay={3.25}>.</LoadingText>
+                    <LoadingText delay={3.5}>.</LoadingText>
+                    <LoadingText delay={3.75}>.</LoadingText>
+                </LoadingItem>
+            </TextBox>
         </Container>
     );
 };
