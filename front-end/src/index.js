db90729374919f4787e364094d9f1aff7ce9f4db
권영재 <dwdjjj@naver.com>
2023-08-08 09:41:19 +0900

    Feat : persist redux setting

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,10 +5,15 @@ import { Provider } from "react-redux";
 import store from "./store";
 import GlobalStyle from "./components/style/GlobalStyle";
 import Modal from "./components/modal/Modal";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}></PersistGate>
         <Modal />
         <App />
         <GlobalStyle />
--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -2,6 +2,17 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { modalSlice } from "./modalSlice";
 import { openviduSlice } from "./openviduSlice";
 import { phaseSlice } from "./phaseSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    // localStorage에 저장
+    storage,
+    // reducer 중에 openvidu reducer만 localstorage에 저장
+    whitelist: ["openvidu"],
+    // blacklist -> 여기 넣은 reducer 제외
+};
 
 const rootReducer = combineReducers({
     modal: modalSlice.reducer,
@@ -9,15 +20,13 @@ const rootReducer = combineReducers({
     phase: phaseSlice.reducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
-            // serializableCheck: {
-            // ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            // ovActions: false,
-            // },
         }),
 });
 
