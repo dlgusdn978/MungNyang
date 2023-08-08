c45c992821aeb10147d222a6c495fc643c0c4684
LeeMinGyu <lmg386411@gmail.com>
2023-08-08 09:27:38 +0900

    Style : Now Loading에 반복문 적용

--- front-end/src/views/Loading.jsx
+++ front-end/src/views/Loading.jsx
@@ -8,27 +8,19 @@ import {
 } from "../components/layout/loading";
 
 const Loading = () => {
+    const loadingText = "Now Loading...";
+
     return (
         <Container>
             <img width={1280} height={670} src={imgSrc} alt="" />
             <TextBox>
                 <TipItem>Tip : 게임시작 전 간단한 스트레칭은 어떤가요?</TipItem>
                 <LoadingItem>
-                    <LoadingText delay={0.25}>N</LoadingText>
-                    <LoadingText delay={0.5}>o</LoadingText>
-                    <LoadingText delay={0.75}>w</LoadingText>
-                    <LoadingText delay={1}>&nbsp;</LoadingText>
-                    <LoadingText delay={1.25}>L</LoadingText>
-                    <LoadingText delay={1.5}>o</LoadingText>
-                    <LoadingText delay={1.75}>a</LoadingText>
-                    <LoadingText delay={2}>d</LoadingText>
-                    <LoadingText delay={2.25}>i</LoadingText>
-                    <LoadingText delay={2.5}>n</LoadingText>
-                    <LoadingText delay={2.75}>g</LoadingText>
-                    <LoadingText delay={3}>&nbsp;</LoadingText>
-                    <LoadingText delay={3.25}>.</LoadingText>
-                    <LoadingText delay={3.5}>.</LoadingText>
-                    <LoadingText delay={3.75}>.</LoadingText>
+                    {loadingText.split("").map((letter, index) => (
+                        <LoadingText key={index} delay={0.1 * (index + 1)}>
+                            {letter}
+                        </LoadingText>
+                    ))}
                 </LoadingItem>
             </TextBox>
         </Container>
