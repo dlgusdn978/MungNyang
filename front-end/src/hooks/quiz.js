a9cc07a598eef56b137d9619a6d017a076d3cd05
lee hyunwoo <dlgusdn978@naver.com>
2023-08-09 15:21:37 +0900

    Style: api code modify

--- front-end/src/hooks/quiz.js
+++ front-end/src/hooks/quiz.js
@@ -3,7 +3,9 @@ import {
     QuizAnswerNegative,
     QuizAnswerPositive,
     QuizResult,
-    liarAnswer,
+    selectCategory,
+    emergencyAnswer,
+    finalAnswer,
 } from "../api/game";
 
 export const fetchQuizInfo = async (roomId) => {
@@ -33,6 +35,50 @@ export const fetchQuizResult = async (roomId, gameId) => {
     try {
         const quizResultresponse = await QuizResult(roomId, gameId);
         return quizResultresponse.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+export const fetchUserRole = async (roomId, gameId, category, answerer) => {
+    try {
+        const userRoleResponse = await selectCategory(
+            roomId,
+            gameId,
+            category,
+            answerer,
+        );
+        console.log(userRoleResponse.data.playersRoleInfo);
+        return userRoleResponse.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const fetchEmergencyAnswerResponse = async (
+    roomId,
+    setId,
+    playerNickname,
+    answer,
+) => {
+    try {
+        const emergencyAnswerResponse = await emergencyAnswer(
+            roomId,
+            setId,
+            playerNickname,
+            answer,
+        );
+        console.log(emergencyAnswerResponse.data);
+        return emergencyAnswerResponse.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const fetchFinalAnswerResponse = async (setId, roomId, answer) => {
+    try {
+        const finalAnswerResponse = await finalAnswer(setId, roomId, answer);
+        console.log(finalAnswerResponse.data);
+        return finalAnswerResponse.data;
     } catch (error) {
         console.log(error);
     }
