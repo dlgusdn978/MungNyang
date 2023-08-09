132d2c36d48fcb3ce227bfe3e86974ff1b2e10f6
lee hyunwoo <dlgusdn978@naver.com>
2023-08-09 17:52:05 +0900

    Style: Game Slice

---
+++ front-end/src/store/gameSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    setId: 0,
+    score: 0,
+    selectedLiar: "",
+    answerer: "",
+    word: "",
+    gameId: 0,
+};
+const initState = {
+    setId: 0,
+    score: 0,
+    selectedLiar: "",
+    answerer: "",
+    word: "",
+    gameId: 0,
+};
+export const gameSlice = createSlice({
+    name: "game",
+    initialState,
+    reducers: {
+        saveSetId: (state, action) => {
+            state.setId = action.setId;
+        },
+        saveScore: (state, action) => {
+            state.score += action.payload;
+        },
+        saveLiar: (state, action) => {
+            state.selectedLiar = action.payload;
+        },
+        saveAnswerer: (state, action) => {
+            state.answerer = action.payload;
+        },
+        saveWord: (state, action) => {
+            state.word = action.word;
+        },
+        initGameState: (state) => {
+            [...state] = [...initState];
+        },
+    },
+});
+
+export const { openModal, closeModal } = gameSlice.actions;
+export const gameActions = gameSlice.actions;
