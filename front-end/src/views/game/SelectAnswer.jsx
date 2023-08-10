49b6ebf21a469f50772b1da64692e85c3d15a6df
LeeMinGyu <lmg386411@gmail.com>
2023-08-10 13:15:03 +0900

    Style : SelectLiar develop에 있는 상태로 변경

--- front-end/src/views/game/SelectAnswer.jsx
+++ front-end/src/views/game/SelectAnswer.jsx
@@ -18,26 +18,6 @@ const SelectAnswer = (props) => {
     const [showNotification, setShowNotification] = useState(true);
     const title = "아래 단어들 중 정답을 골라주세요.";
     const text = "라이어로 지목되었습니다. ";
-    const Answerlist = [
-        "고구마",
-        "토마토",
-        "날치알",
-        "짜게치",
-        "신라면",
-        "너구리",
-        "양파링",
-        "복숭아",
-        "사이다",
-        "고구마1",
-        "고구마2",
-        "고구마3",
-        "고구마4",
-        "고구마5",
-        "고구마6",
-        "고구마7",
-        "고구마8",
-        "고구마9",
-    ];
     const imgSrc = foot2;
     const [answerList, setAnswerList] = useState([]);
     useEffect(() => {
@@ -59,7 +39,7 @@ const SelectAnswer = (props) => {
     };
     return (
         <Container>
-            <Timer></Timer>
+            <Timer time={time}></Timer>
             <Head>{title}</Head>
             <Line>
                 {answerList.map((item, index) => (
