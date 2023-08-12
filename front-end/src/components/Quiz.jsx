41c98667633e5231f6cad2a0d3cbcae5e2414659
권영재 <dwdjjj@naver.com>
2023-08-12 13:27:28 +0900

    Style : css fix

--- front-end/src/components/Quiz.jsx
+++ front-end/src/components/Quiz.jsx
@@ -1,66 +1,24 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
 import Timer from "./Timer";
 import { useEffect } from "react";
 import { fetchQuizResult, submitAnswer } from "../hooks/quiz";
 import { gameActions } from "../store/gameSlice";
-
-const Container = styled.div`
-    padding: 20px;
-    padding-bottom: 40px;
-    width: 600px;
-    height: 300px;
-    background: var(--macciato);
-    text-align: center;
-    border-radius: 5px;
-`;
-
-const Title = styled.div`
-    padding: 20px;
-    margin-bottom: 20px;
-    text-align: center;
-    font-size: 32px;
-    color: var(--dusty-pink-dark);
-`;
-
-const Box = styled.div`
-    display: flex;
-`;
-
-const Content = styled.button`
-    background-color: var(--white);
-    margin-right: 10px;
-    margin-left: 10px;
-    width: 350px;
-    height: 150px;
-    border-radius: 5px;
-    font-size: 32px;
-    color: var(--beige-dark);
-    box-shadow: 3px 3px 3px var(--blacks);
-    transition-duration: 0.3s;
-    :active {
-        margin-left: 15px;
-        margin-top: 15px;
-        box-shadow: none;
-    }
-    :hover {
-        cursor: pointer;
-    }
-`;
+import { Container, Content, FlexBox, Title } from "./layout/quiz";
 
 const Quiz = (props) => {
     const { title, text1, text2, onViewChange, ChooseModal } = props;
+    const { mySessionId, myUserName, owner } = openvidu;
     const [showChooseModal, setShowChooseModal] = useState(false);
     const [answerer, setAnswerer] = useState("");
     const [answered, setAnswered] = useState(false);
     const [userChoice, setUserChoice] = useState("");
+    const [quizResultFetched, setQuizResultFetched] = useState(false);
     const dispatch = useDispatch();
     const openvidu = useSelector((state) => state.openvidu);
-    const { mySessionId, myUserName, owner } = openvidu;
     const roomId = mySessionId;
     const playerNickname = myUserName;
-    const [quizResultFetched, setQuizResultFetched] = useState(false);
+
     const handleUserChoice = (isPositive) => {
         setUserChoice(isPositive ? "positive" : "negative");
     };
@@ -107,13 +65,9 @@ const Quiz = (props) => {
     }, [setShowChooseModal, onViewChange, showChooseModal]);
     return (
         <Container>
-            <Timer
-                onTimerEnd={() => setAnswered(true)}
-                roomId={roomId}
-                myUserName={myUserName}
-            />
+            <Timer onTimerEnd={() => setAnswered(true)} />
             <Title>{title}</Title>
-            <Box>
+            <FlexBox>
                 <Content
                     onClick={() => handleUserChoice(true)}
                     clicked={userChoice}
@@ -126,7 +80,7 @@ const Quiz = (props) => {
                 >
                     {text2}
                 </Content>
-            </Box>
+            </FlexBox>
             {quizResultFetched && <ChooseModal answerer={answerer} />}
         </Container>
     );
