2f110f73aa33b12ca55814a672063e5fdac3d056
LeeMinGyu <lmg386411@gmail.com>
2023-08-16 00:37:31 +0900

    Patch : 라이어 지목 타이머 방식 변경 / 점수 집계 css

--- front-end/src/views/game/LiarVote.jsx
+++ front-end/src/views/game/LiarVote.jsx
@@ -16,81 +16,62 @@ import { gameActions } from "../../store/gameSlice";
 
 const LiarVote = () => {
     const openvidu = useSelector((state) => state.openvidu);
-    const { session, publisher } = openvidu;
+    const { session, publisher, owner } = openvidu;
     const setId = useSelector((state) => state.game.setId);
     const [showNotification, setShowNotification] = useState(true);
-    const [activeBox, setActiveBox] = useState(null);
     const text = "라이어를 선택하세요.";
     const imgSrc = foot;
     const dispatch = useDispatch();
     const roomId = useSelector((state) => state.openvidu.mySessionId);
     const answerer = useSelector((state) => state.game.answerer);
+    const [answered, setAnswered] = useState(false);
+    const [activeBox, setActiveBox] = useState(null);
+    const [next, setNext] = useState(false);
+
+    const handleBoxClick = (name) => {
+        setActiveBox(name === activeBox ? null : name);
+    };
 
     useEffect(() => {
-        const timer = setTimeout(async () => {
+        const handleSubmission = async () => {
             try {
                 const response = await selectLiar(setId, activeBox);
-                console.log(response);
-                setTimeout(async () => {
-                    try {
-                        const selectedLiarResponse = await selectedLiar(setId);
-                        console.log(selectedLiarResponse);
-                        if (
-                            selectedLiarResponse.data.gameProcessType ===
-                            "LiarVote"
-                        ) {
-                            const dupliars =
-                                selectedLiarResponse.data.mostVotedNicknames;
-                            console.log(dupliars);
-                            dispatch(gameActions.updateDupLiars(dupliars));
-                            dispatch(changePhase("DupLiar"));
-                        } else if (
-                            selectedLiarResponse.data.gameProcessType ===
-                            "SelectAns"
-                        ) {
-                            const mostVotedNickname =
-                                selectedLiarResponse.data.mostVotedNicknames[0];
-                            console.log(mostVotedNickname);
-                            dispatch(
-                                gameActions.updateSelectedLiar(
-                                    mostVotedNickname,
-                                ),
-                            );
-                            if (
-                                publisher.stream.connection.data ===
-                                mostVotedNickname
-                            ) {
-                                dispatch(changePhase("SelectAns"));
-                            } else {
-                                dispatch(changePhase("OtherView"));
-                            }
-                        } else {
-                            try {
-                                const response = await Result(
-                                    setId,
-                                    roomId,
-                                    0,
-                                    0,
-                                );
-                                const result = response.data;
-                                console.log(result);
 
-                                dispatch(gameActions.updateResult(result));
-                                dispatch(changePhase("MidScore"));
-                            } catch (error) {
-                                console.error(error);
-                            }
-                        }
-                    } catch (error) {
-                        console.error(error);
-                    }
-                }, 2000);
+                console.log(setId);
+                console.log(response);
             } catch (error) {
-                console.error(error);
+                console.error("Error", error);
             }
-        }, 10000);
-        return () => clearTimeout(timer);
-    });
+        };
+        if (answered) {
+            handleSubmission();
+            setNext(true);
+        }
+    }, [answered, activeBox]);
+
+    useEffect(() => {
+        const handleResult = async () => {
+            try {
+                const selectedLiarResponse = await selectedLiar(setId);
+                console.log(selectedLiarResponse);
+                const mostVotedNickname =
+                    selectedLiarResponse.data.mostVotedNicknames[0];
+                console.log(mostVotedNickname);
+                dispatch(gameActions.updateSelectedLiar(mostVotedNickname));
+
+                if (publisher.stream.connection.data === mostVotedNickname) {
+                    dispatch(changePhase("SelectAns"));
+                } else {
+                    dispatch(changePhase("OtherView"));
+                }
+            } catch (error) {
+                console.error("Error", error);
+            }
+        };
+        if (next) {
+            handleResult();
+        }
+    }, [next]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -102,13 +83,9 @@ const LiarVote = () => {
         };
     }, []);
 
-    const handleBoxClick = (name) => {
-        setActiveBox(name === activeBox ? null : name);
-    };
-
     return (
         <Container>
-            <Timer></Timer>
+            <Timer onTimerEnd={() => setAnswered(true)} />
             <Box>
                 {session.streamManagers &&
                     session.streamManagers.map((subscriber, i) => {
@@ -147,6 +124,8 @@ const LiarVote = () => {
                                                 activeBox ===
                                                 subscriber.stream.connection
                                                     .data
+                                                    ? true
+                                                    : false
                                             }
                                         >
                                             <img
